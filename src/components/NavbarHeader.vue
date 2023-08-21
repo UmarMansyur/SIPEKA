ab6fd44f7891d65bc66031d6a7a5a8017114c4ed
Muhammad Umar Mansyur <umar.ovie@gmail.com>
2023-08-21 17:35:58 +0700

    Push

--- src/components/NavbarHeader.vue
+++ src/components/NavbarHeader.vue
@@ -5,19 +5,19 @@
       <div class="navbar-brand-box">
         <a href="index.html" class="logo logo-dark">
           <span class="logo-sm">
-            <img src="assets/images/logo-sm.svg" alt="" height="30">
+            <img src="/assets/images/logo-sm.svg" alt="" height="30">
           </span>
           <span class="logo-lg">
-            <img src="assets/images/logo-sm.svg" alt="" height="24"> <span class="logo-txt">SIPEKA</span>
+            <img src="/assets/images/logo-sm.svg" alt="" height="24"> <span class="logo-txt">SIPEKA</span>
           </span>
         </a>
 
         <a href="index.html" class="logo logo-light">
           <span class="logo-sm">
-            <img src="assets/images/logo-sm.svg" alt="" height="30">
+            <img src="/assets/images/logo-sm.svg" alt="" height="30">
           </span>
           <span class="logo-lg">
-            <img src="assets/images/logo-sm.svg" alt="" height="24"> <span class="logo-txt">SIPEKA</span>
+            <img src="/assets/images/logo-sm.svg" alt="" height="24"> <span class="logo-txt">SIPEKA</span>
           </span>
         </a>
       </div>
